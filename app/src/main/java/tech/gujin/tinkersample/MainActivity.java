c8041149bbe19ad2fc05e7e8fb89b30024d7821d
gujin_ <gujin@txyapp.com>
2017-09-12 16:58:18 +0800

    Initial commit

--- app/src/main/java/tech/gujin/tinkersample/MainActivity.java
+++ app/src/main/java/tech/gujin/tinkersample/MainActivity.java
@@ -1,13 +1,27 @@
 package tech.gujin.tinkersample;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.tv_msg)).setText("此版本存在BUG");
+
+        File file = new File(getExternalCacheDir(), "/patch_signed_7zip.apk");
+        if (file.exists()) {
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), file.getAbsolutePath());
+        }
     }
 }
--- app/src/main/java/tech/gujin/tinkersample/application/MyApplicationLike.java
+++ app/src/main/java/tech/gujin/tinkersample/application/MyApplicationLike.java
@@ -1,16 +1,30 @@
 package tech.gujin.tinkersample.application;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 
 import com.tencent.tinker.anno.DefaultLifeCycle;
-import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
-@DefaultLifeCycle(application = "tech.gujin.application.MyApplication", flags = ShareConstants.TINKER_ENABLE_ALL)
-public class MyApplicationLike extends ApplicationLike {
+@DefaultLifeCycle(application = ".MyApplication", flags = ShareConstants.TINKER_ENABLE_ALL)
+public class MyApplicationLike extends DefaultApplicationLike {
 
     public MyApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        TinkerInstaller.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 原有Application逻辑可以写在这里
     }
 }
